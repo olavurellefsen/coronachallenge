da06cc075ef6fb1d8021370be1cf42754b6a0b7a
olavurellefsen <oe@tokni.com>
2020-05-19 22:04:49 +0100

    Update meta information

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `North Atlantic Corona Challenge`,
+    description: `A 48-hour online challenge to tackle the Covid-19 Crisis`,
+    author: `@tokni`,
   },
   plugins: [
     `styled-components`,
