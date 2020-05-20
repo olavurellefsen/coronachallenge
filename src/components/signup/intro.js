11068cba456293ef5bf23f6402005b3fecd5a0d3
olavurellefsen <oe@tokni.com>
2020-05-20 11:44:55 +0100

    Change from /register to /signup

--- src/components/signup/intro.js
+++ src/components/signup/intro.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 
 const Intro = () => (
   <IntroContainer>
-    <IntroTitleStyle>REGISTER</IntroTitleStyle>
+    <IntroTitleStyle>SIGN UP</IntroTitleStyle>
   </IntroContainer>
 )
 
