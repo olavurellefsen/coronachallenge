7e8ae70671a6488abaed71ad7545fe14533b5e96
olavurellefsen <oe@tokni.com>
2020-05-23 17:36:43 +0100

    Only show confirmation if submitted

--- src/pages/registered.js
+++ src/pages/registered.js
@@ -1,25 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import React from "react"
+import styled from "styled-components"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-const registered = () => (
-  <Layout>
-    <SEO title="registered" />
-    <ContainerStyle>
-      <TextStyle>
-        You've succesfully registered. Please check your email, and follow the instructions listed there. Possibly check spam box for it.
-        </TextStyle>
-    </ContainerStyle>
-  </Layout>
-)
-const ContainerStyle = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
+const Registered = ({ location }) => {
+  return (
+    <Layout>
+      {location.state ? (
+        location.state.requestId === `2009906c-a06f-4aee-b0ea-a38b00c5779c` && (
+          <>
+            <SEO title="Sign up received" />
+            <TextStyle>Sign up received.</TextStyle>
+            <TextStyle>
+              Please check your email and follow the instructions to confirm
+              your sign up.
+            </TextStyle>
+            <TextStyle>
+              You might want to check your spam folder if you cannot find the
+              email.
+            </TextStyle>
+          </>
+        )
+      ) : (
+        <TextStyle>Sign up confirmation page.</TextStyle>
+      )}
+    </Layout>
+  )
+}
+
 const TextStyle = styled.div`
   font-size: 22px;
-  margin-top: 20px;
+  text-align: center;
+  margin: 20px;
 `
-export default registered
+
+export default Registered
