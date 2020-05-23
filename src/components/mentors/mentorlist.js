660af9f03daf964c5b8d01ef40f66d1db3e52833
olavurellefsen <oe@tokni.com>
2020-05-23 10:39:04 +0100

    Add mentor page

---
+++ src/components/mentors/mentorlist.js
@@ -0,0 +1,67 @@
+export const mentorlist = [
+  {
+    name: "Hans Fredrik Sørdal",
+    country: "Norway",
+    position:
+      "Previous mayor Flakstad municipality (Lofoten Islands), soon to be CEO for Nordland County Fishermen's Association",
+    image: "hansfredrik.jpg",
+  },
+  {
+    name: "Sigrun Jónsveinsdóttir",
+    country: "Faroe Islands",
+    position: "Leader for Iverksetarahúsið Klaksvik",
+    image: "sigrun.png",
+  },
+  {
+    name: "Rani Nolsøe",
+    country: "Faroe Islands",
+    position: "Owner, Fjallaferðir (Mountain Tours)",
+    image: "rani.jpg",
+  },
+  {
+    name: "Espen Berg-Larsen",
+    country: "Norway",
+    position: "CFO, Brim Explorer",
+  },
+  {
+    name: "Odd Jarl Borch",
+    country: "Norway",
+    position: "Professor, Nord University, leader NORDLAB",
+  },
+  {
+    name: "Erlend Bullvåg",
+    country: "Norway",
+    position: "Dean, Nord University Business School",
+  },
+  {
+    name: "Anja Salo",
+    country: "Norway",
+    position: "Communication director, Polar institute",
+  },
+  {
+    name: "Ingi Vífill Guðmundsson",
+    country: "Iceland",
+    position:
+      "Project manager, social media marketing, Innovation Center Iceland",
+  },
+  {
+    name: "Sóley Heradóttir Hammer",
+    country: "Faroe Islands",
+    position: "Leader, Hugskotid Torshavn",
+  },
+  {
+    name: "Fida Abu Libdeh",
+    country: "Iceland",
+    position: "Founder and CEO, Geosolica",
+  },
+  {
+    name: "Snjólaug Ólafsdóttir",
+    country: "Iceland",
+    position: "Founder, Andrými sustainability center",
+  },
+  {
+    name: "Jens-Jakob Lennert-Sandgreen",
+    country: "Greenland",
+    position: "Business Development Consultant, Greenland Business",
+  },
+]
