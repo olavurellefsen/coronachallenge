39a6f31fd9be8c11069eb0421239732c32bb7251
olavurellefsen <oe@tokni.com>
2020-05-26 20:43:26 +0100

    Add schedule page

---
+++ src/components/schedule/header.js
@@ -0,0 +1,46 @@
+import React from "react"
+import Background from "./background"
+import styled from "styled-components"
+
+const Header = () => (
+  <HeaderContainer href="/">
+    <Background />
+    <ProjectTitleStyle>North Atlantic Corona Challenge</ProjectTitleStyle>
+    <Tag1Style>#CORONACHALLENGE</Tag1Style>
+  </HeaderContainer>
+)
+
+const HeaderContainer = styled.a`
+  position: static;
+  width: 100%;
+`
+
+const ProjectTitleStyle = styled.div`
+  position: absolute;
+  top: 300px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  color: white;
+  background: #006eb6;
+  font-size: 28px;
+  font-weight: 600;
+  opacity: 0.9;
+  padding: 16px 20px;
+  text-transform: uppercase;
+  text-align: center;
+`
+
+const Tag1Style = styled.div`
+  position: absolute;
+  top: 96px;
+  right: 5%;
+  transform: rotate(20deg);
+  color: white;
+  background: #006eb6;
+  font-size: 20px;
+  opacity: 1;
+  padding: 12px 26px;
+  text-transform: uppercase;
+`
+
+export default Header
