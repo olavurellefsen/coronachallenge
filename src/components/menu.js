3eef92ae80cd3ea498769e2799d73f5c8e8df035
olavurellefsen <oe@tokni.com>
2020-05-27 00:05:20 +0100

    Rearrange menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -6,37 +6,37 @@ const BurgerMenu = () => (
     <a id="home" className="menu-item" href="/">
       Home
     </a>
+    <a id="video" className="menu-item" href="/video">
+      Intro Video
+    </a>
     <a id="signup" className="menu-item" href="/signup">
       Sign up
     </a>
     <a id="schedule" className="menu-item" href="/schedule">
       Schedule
     </a>
-    <a id="inspiration" className="menu-item" href="/inspiration">
-      Inspiration
-    </a>
     <a id="speaker" className="menu-item" href="/speaker">
-      Speaker
-    </a>
-    <a id="video" className="menu-item" href="/video">
-      Intro video
-    </a>
-    <a id="media" className="menu-item" href="/media">
-      Media coverage
-    </a>
-    <a id="jury" className="menu-item" href="/jury">
-      Jury
+      Speaker: <br/>Heini Zachariassen
     </a>
     <a id="mentors" className="menu-item" href="/mentors">
       Mentors
     </a>
-    <a id="privacypolicy" className="menu-item" href="/privacypolicy">
-      Privacy Policy
+    <a id="inspiration" className="menu-item" href="/inspiration">
+      Previous Winners
     </a>
     <a id="rules" className="menu-item" href="/rules">
       Rules and
       <br />
       Code of Conduct
+    </a>
+    <a id="jury" className="menu-item" href="/jury">
+      Jury Members
+    </a>
+    <a id="privacypolicy" className="menu-item" href="/privacypolicy">
+      Privacy Policy
+    </a>
+    <a id="media" className="menu-item" href="/media">
+      Media Coverage
     </a>
     <a id="contactus" className="menu-item" href="#aboutus">
       Contact us
