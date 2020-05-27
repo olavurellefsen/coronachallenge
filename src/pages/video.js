37f2c8c08e6adf4f354a9fb109519cec99bcef5a
olavurellefsen <oe@tokni.com>
2020-05-27 11:15:03 +0100

    Remove intro text and buttons

--- src/pages/video.js
+++ src/pages/video.js
@@ -3,7 +3,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from "../components/video/header"
 import Video from "../components/video/video"
-import Intro from "../components/video/intro"
 import AboutUs from "../components/footer/about-us"
 import Image from "../images/girl-smartphone-beach-dark-1200x630.jpg"
 
@@ -16,7 +15,6 @@ const Page = () => (
     />
     <Header />
     <Video />
-    <Intro />
     <AboutUs />
   </Layout>
 )
